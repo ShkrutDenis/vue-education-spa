7f90d2a88461f3402d6ff85c2d30784dec6ebcc3
Denis Shkrut <Shkrut_denis@rambler.ru>
2021-09-01 12:44:34 +0300

    Component structure

---
+++ src/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="example">Example</div>
+</template>
+
+<script>
+export default {
+  name: "Example",
+};
+</script>
+
+<style scoped>
+.example {
+  color: red;
+}
+</style>

1c0bbbc9c1799e16056ae29763b398259ade20f6
Denis Shkrut <Shkrut_denis@rambler.ru>
2021-09-11 17:30:18 +0300

    Add route for the our Example component

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="example">Example</div>
+  <div class="example" @click="test">Example</div>
 </template>
 
 <script>
 export default {
   name: "Example",
+  methods: {
+    test() {
+      this.$router.push({ name: "Home" });
+    }
+  }
 };
 </script>
 

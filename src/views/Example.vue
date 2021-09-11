0d4193f9a0f2a76cab245ec774e9cf8192696fdc
Denis Shkrut <Shkrut_denis@rambler.ru>
2021-09-11 19:13:57 +0300

    Binding

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="example" @click="test">Example</div>
+    <div :title="'Jump to: ' + $router.getRoutes()[0].name"
+         class="example" @click="$router.push({ name: 'Home' })">{{ 'Example' }}</div>
+
     <card :title="'Title'" :value="5" :data="{ a: 1, b: 2 }"></card>
   </div>
 </template>
@@ -11,11 +13,6 @@ import Card from "../components/Card";
 export default {
   name: "Example",
   components: { Card },
-  methods: {
-    test() {
-      this.$router.push({ name: "Home" });
-    },
-  },
 };
 </script>
 
